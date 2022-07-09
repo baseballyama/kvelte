e3a66e4d67e544cd3152ab3260d317e53b9cf59c
Yuichiro Yamashita <xydybaseball@gmail.com>
2022-07-10 01:14:18 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 set -e
 
-rm -rf ./kvelte
+cd ..
+rm -rf kvelte
 git clone https://github.com/baseballyama/kvelte.git
 cd kvelte
+
+cd ./kvelte-node
+npm i
+npm run build
+npm link
+cd ..
+
+cd ./kvelte
 ./gradlew build
+cd ..
+
+cd ./examples/ktor/src/main/resources/kvelte
+npm i
+npm link kvelte-node
+cd ../../../..
+
+./gradlew build
+cd ../..
+
 java -jar examples/ktor/build/libs/kvelte-demo.jar
